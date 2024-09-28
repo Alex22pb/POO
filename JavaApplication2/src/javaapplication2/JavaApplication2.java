b9148b01deb29ddbee9c4aa74171beb38a305c18
Alex22pb <alexis22pb@gmail.com>
2024-09-28 00:07:40 -0300

    Alterações feitas

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,13 +1,9 @@
-
 package javaapplication2;
-
-
 
 import helpMe.Arena;
 import static helpMe.Arena.iniciarArray;
 import java.io.IOException;
 import helpMe.QuestoesTrabalho;
-import testejava.AparecerQuestoes;
 
 
 public class JavaApplication2 {
@@ -16,19 +12,24 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {        
-            
+        try {
+
             iniciarArray(); // Certifique-se de que isso inicializa todas as listas de guerreiros necessárias
 
             // Chamadas para exibir dados de guerreiros, maior peso e guerreiro mais velho
-            AparecerQuestoes.exibirDados();
-            AparecerQuestoes.exibirPeso();
-            QuestoesTrabalho.maisVelho(Arena.getArena()); 
+            System.out.println("\nDados dos Guerreiros\n");
+            System.out.println("=============Guerreiros Gregos e Nórdicos=============");
+            QuestoesTrabalho.exibirDadosGuerreiros(Arena.getArena());
+            
+            double maiorPeso = QuestoesTrabalho.somaPesos(Arena.getArena());
+            System.out.println("\nA soma dos pesos de todos os guerreiros e: " + maiorPeso);
+            
+            QuestoesTrabalho.maisVelho(Arena.getArena());
 //            
 
         } catch (IOException e) {
             System.err.println("Ocorreu um erro ao inicializar ou exibir os dados dos guerreiros: " + e.getMessage());
         }
     }
-    
+
 }
